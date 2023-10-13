0c1e0fcea8747e1760ed162d09f6364f465e65e9
Joe Lewis <walesrugby709500@gmail.com>
2023-10-13 10:06:19 +0100

    feat: add search requests by id and print results

--- run.py
+++ run.py
@@ -19,15 +19,15 @@ location = SHEET.worksheet('location')
 
 def find_request_menu():
     """
-    To search and display request information
+    To select which search you would like to do on a request
     """
     menu=True
 
     while menu:
         print('1. Search by request ID\n2. Search by received date\n3. Search by completed date\n4. Search by type')
-        selection = input('Please enter the number of what you would like to search by ')
+        selection = input('Please enter the number of what you would like to search by \n')
         if selection == '1':
-            print('Selected 1')
+            find_request_by_id()
             menu = False
         elif selection == '2':
             print('Selected 2')
@@ -41,4 +41,27 @@ def find_request_menu():
         else:
             print(f'Invalid selection: You selected {selection} please try again')
 
+def find_request_by_id():
+    """
+    Get request details from id number
+    """
 
+    id = input('What is the request id you would like to view? \n')
+    # Finds the id and stores what cell that id is in
+    cell = requests.find(id,in_column=1)
+
+    row = cell.row
+
+    get_request_details(id,row)
+
+def get_request_details(id,row):
+
+    date_received = requests.cell(row,4).value
+    date_completed = requests.cell(row,5).value
+    text = requests.cell(row,6).value
+    type = requests.cell(row,7).value
+    time_to_complete = requests.cell(row,8).value
+
+    print(f'Request ID: {id}\nDate Received: {date_received}\nDate Completed: {date_completed}\nRequest Details: {text}\nType: {type}\nTime to complete: {time_to_complete} days')
+
+find_request_menu()
