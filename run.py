982c8022e6c1534398154d8f7def0e822934b684
Joe Lewis <walesrugby709500@gmail.com>
2023-10-13 09:37:35 +0100

    feat: add find request menu

--- run.py
+++ run.py
@@ -17,7 +17,28 @@ actions = SHEET.worksheet('actions')
 contact = SHEET.worksheet('contact')
 location = SHEET.worksheet('location')
 
-print(requests)
-print(actions)
-print(contact)
-print(location)
+def find_request_menu():
+    """
+    To search and display request information
+    """
+    menu=True
+
+    while menu:
+        print('1. Search by request ID\n2. Search by received date\n3. Search by completed date\n4. Search by type')
+        selection = input('Please enter the number of what you would like to search by ')
+        if selection == '1':
+            print('Selected 1')
+            menu = False
+        elif selection == '2':
+            print('Selected 2')
+            menu = False
+        elif selection == '3':
+            print('Selected 3')
+            menu = False
+        elif selection == '4':
+            print('Selected 4')
+            menu = False
+        else:
+            print(f'Invalid selection: You selected {selection} please try again')
+
+
